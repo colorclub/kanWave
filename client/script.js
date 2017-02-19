5afbe2adadb99a1668ddc31361409a8729e40909
Timothy Yin <tyin92@gmail.com>
2017-02-18 20:15:22 -0500

    bounce success

--- client/script.js
+++ client/script.js
@@ -7,16 +7,16 @@ function bounce(singer) {
 	let onComplete = function() {
 		console.log('ON COMPLETE')
 		if(bottom) {
-			let width = screen.width/2 - 400;
-			let height = screen.height/2 - 200;
+			width = screen.width/2 - 400;
+			height = screen.height/2 - 110;
 			bottom = !bottom;
 		}
 		else{
-			let width = screen.width/2 - 400;
-			let height = screen.height/2 - 100;
+			width = screen.width/2 - 400;
+			height = screen.height/2 - 100;
 			bottom = !bottom;
 		}
-		TweenLite.to(document.getElementById(singer), 1, {x: width, y: height, onComplete: onComplete});
+		TweenLite.to(document.getElementById(singer), .2, {x: width, y: height, onComplete: onComplete});
 	}
 	TweenLite.to(document.getElementById(singer), 1.5, {x: width, y: height, onComplete: onComplete});
 }
