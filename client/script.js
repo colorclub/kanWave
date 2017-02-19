0b4447f56c432750270d5cceb58b9464b5566c6b
Timothy Yin <tyin92@gmail.com>
2017-02-18 19:23:42 -0500

    Resolve merge conflict

--- client/script.js
+++ client/script.js
@@ -1,3 +1,24 @@
-window.onload = function (){
-	TweenLite.to(document.getElementById("kanye"), 2.5, {ease: Bounce.easeOut, x: 640});
+
+let bottom = true;
+
+function bounce(singer) {
+	let width = Math.random() * screen.width;
+	let height;
+
+	if(bottom) {
+		height = screen.height - 25;
+		bottom = !bottom;
+	}
+	else{
+		height = 25;
+		bottom = !bottom;
+	}
+	TweenLite.to(document.getElementById(singer), 2.5, {x: width, y: height});
+	setInterval(function() {
+		bounce(singer);
+	}, 2700);
+}
+
+window.onload = function() {
+	bounce("kanye");
 }
